91fe529b1fa9f2d640f9512c30b25ce1ea77aee9
Rielk <william@symbotics.co.uk>
2022-12-08 10:55:09 +0000

    Day 6 Part 1 - Complete

---
+++ AdventOfCode22Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode22Day6.Properties;
+
+string input = Resources.Input1;
+
+int countToMarker = FindXUnique(4);
+
+Console.WriteLine($"Characters processed before the first marker: {countToMarker}");
+Console.WriteLine();
+
+int FindXUnique(int n)
+{
+    int countToMarker = 0;
+    List<char> buffer = new();
+
+    foreach (char c in input)
+    {
+        countToMarker++;
+        buffer.Add(c);
+        while (buffer.Count > n)
+            buffer.RemoveAt(0);
+        if (buffer.Distinct().Count() == n)
+            break;
+    }
+
+    return countToMarker;
+}
