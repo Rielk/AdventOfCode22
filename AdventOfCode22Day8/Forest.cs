982334f6b1f51df4b3b3e9e657a8754bb7a77b76
Rielk <william@symbotics.co.uk>
2022-12-08 05:45:31 +0000

    Day 8 Part 1 - Complete

---
+++ AdventOfCode22Day8/Forest.cs
@@ -0,0 +1,93 @@
+﻿namespace AdventOfCode22Day8;
+internal class Forest
+{
+    public int Width { get; }
+    public int Height { get; }
+
+    public Tree[][] TreeArray { get; }
+
+    public Forest(int[][] input)
+    {
+        Width = input.Length;
+        Height = input.First().Length;
+        TreeArray = new Tree[Height][];
+        foreach ((int[] RowValues, int rowIndex) in input.Select((x, i) => (x, i)))
+        {
+            TreeArray[rowIndex] = new Tree[Width];
+            foreach ((int Value, int columnIndex) in RowValues.Select((x, i) => (x, i)))
+            {
+                TreeArray[rowIndex][columnIndex] = new(Value);
+            }
+        }
+
+        foreach (Tree[] row in TreeArray)
+        {
+            //Look from left
+            int canSee = 0;
+            foreach (Tree tree in row)
+            {
+                if (tree.Height >= canSee)
+                {
+                    tree.SetVisibility(true);
+                    canSee = tree.Height + 1;
+                    if (canSee >= 10) break;
+                }
+            }
+
+            //Look from right
+            canSee = 0;
+            foreach (Tree tree in row.Reverse())
+            {
+                if (tree.Height >= canSee)
+                {
+                    tree.SetVisibility(true);
+                    canSee = tree.Height + 1;
+                    if (canSee >= 10) break;
+                }
+            }
+        }
+
+        for (int i = 0; i < Width; i++)
+        {
+            List<Tree> columnList = new();
+            foreach (Tree[] row in TreeArray)
+                columnList.Add(row[i]);
+            Tree[] column = columnList.ToArray();
+
+            //Look from top
+            int canSee = 0;
+            foreach (Tree tree in column)
+            {
+                if (tree.Height >= canSee)
+                {
+                    tree.SetVisibility(true);
+                    canSee = tree.Height + 1;
+                    if (canSee >= 10) break;
+                }
+            }
+
+            //Look from bottom
+            canSee = 0;
+            foreach (Tree tree in column.Reverse())
+            {
+                if (tree.Height >= canSee)
+                {
+                    tree.SetVisibility(true);
+                    canSee = tree.Height + 1;
+                    if (canSee >= 10) break;
+                }
+            }
+        }
+    }
+
+    internal int CountVisible()
+    {
+        int count = 0;
+        foreach (Tree[] x in TreeArray)
+            foreach (Tree y in x)
+                if (y.Visible)
+                    count++;
+        return count;
+    }
+
+}
---
+++ AdventOfCode22Day8/Program.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode22Day8;
+using AdventOfCode22Day8.Properties;
+
+string input = Resources.Input1;
+
+int[][] array = input.Split(Environment.NewLine).Select(l => l.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
+Forest Forest = new(array);
+
+Console.WriteLine($"Number of visible Trees: {Forest.CountVisible()}");
+Console.WriteLine();
