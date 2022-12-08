bd415859f1acf58c3ecc8b0897975afc0cccae3a
Rielk <william@symbotics.co.uk>
2022-12-08 10:45:59 +0000

    Day 5 Part 2 - Complete

--- AdventOfCode22Day5/Program.cs
+++ AdventOfCode22Day5/Program.cs
@@ -38,3 +38,36 @@ foreach (List<char> pile in Stack)
 }
 
 Console.WriteLine();
+
+Stack = new List<char>[9];
+for (int i = 0; i < 9; i++)
+    Stack[i] = new();
+
+foreach (string line in stackInput.Split(Environment.NewLine).Reverse())
+    foreach ((char letter, int index) in line.Chunk(4).Select((x, i) => (x[1], i)))
+        if (letter != ' ')
+            Stack[index].Add(letter);
+
+foreach (string order in moveInput.Split(Environment.NewLine))
+{
+    string[] parts = order.Split(' ');
+    int count = int.Parse(parts[1]);
+    int fromI = int.Parse(parts[3]);
+    int toI = int.Parse(parts[5]);
+
+    List<char> fromPile = Stack[fromI - 1];
+    List<char> toPile = Stack[toI - 1];
+
+    char[] movedChars = fromPile.TakeLast(count).ToArray();
+    for (int i = 0; i < count; i++)
+        fromPile.RemoveAt(fromPile.Count - 1);
+
+    toPile.AddRange(movedChars);
+}
+
+foreach (List<char> pile in Stack)
+{
+    foreach (char c in pile)
+        Console.Write($"[{c}] ");
+    Console.WriteLine();
+}
