4a8e6f1090c642eed5fe211b4bac2de28b1a5c7e
Rielk <william@symbotics.co.uk>
2022-12-11 08:09:16 +0000

    Day 11 Part 1 - Test Working

---
+++ AdventOfCode22Day11/Monkey.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode22Day11;
+internal class Monkey
+{
+    public Monkey(Dictionary<int, Monkey> monkeys, int thisMonkey, List<int> items, Operation operation, int operationFactor, int test, int trueTarget, int falseTarget)
+    {
+        Monkeys = monkeys;
+        if (Monkeys.ContainsKey(thisMonkey)) throw new ArgumentException(nameof(thisMonkey), "A monkey of this number already exists");
+        Monkeys[thisMonkey] = this;
+
+        Items = items;
+        Operation = operation;
+        OperationFactor = operationFactor;
+        Test = test;
+        TrueTarget = trueTarget;
+        FalseTarget = falseTarget;
+    }
+
+    public List<int> Items { get; } = new();
+    public Operation Operation { get; }
+    public int OperationFactor { get; }
+    public int Test { get; }
+    public int TrueTarget { get; }
+    public int FalseTarget { get; }
+
+    public Dictionary<int, Monkey> Monkeys { get; }
+
+    public int InspectionCount { get; private set; }
+
+    public void InspectAll()
+    {
+        foreach (int item in Items.ToArray())
+        {
+            InspectionCount++;
+            Items.RemoveAt(0);
+            int start = Operation switch
+            {
+                Operation.Add => item + OperationFactor,
+                Operation.Mul => item * OperationFactor,
+                Operation.Square => item * item,
+                _ => throw new NotImplementedException(),
+            };
+            start /= 3;
+
+            Monkeys[start % Test == 0 ? TrueTarget : FalseTarget].Items.Add(start);
+        }
+    }
+}
+
+internal enum Operation
+{
+    Add, Mul, Square
+}
---
+++ AdventOfCode22Day11/Program.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode22Day11;
+
+Dictionary<int, Monkey> Monkeys = new();
+
+_ = new Monkey(Monkeys, 0, new() { 79, 98 }, Operation.Mul, 19, 23, 2, 3);
+_ = new Monkey(Monkeys, 1, new() { 54, 65, 75, 74 }, Operation.Add, 6, 19, 2, 0);
+_ = new Monkey(Monkeys, 2, new() { 79, 60, 97 }, Operation.Square, 0, 13, 1, 3);
+_ = new Monkey(Monkeys, 3, new() { 74 }, Operation.Add, 3, 17, 0, 1);
+
+for (int n = 0; n < 20; n++)
+    for (int i = 0; i < Monkeys.Count; i++)
+    {
+        Monkey monkey = Monkeys[i];
+        monkey.InspectAll();
+    }
+
+for (int i = 0; i < Monkeys.Count; i++)
+{
+    Console.WriteLine($"Monkey {i} inspected items {Monkeys[i].InspectionCount} times.");
+}
+
+int MonkeyBusiness = Monkeys.Values.Select(m => m.InspectionCount).OrderByDescending(x => x).Take(2).Aggregate(1, (x, y) => x * y);
+
+Console.WriteLine();
+Console.WriteLine($"Monkey Business is: {MonkeyBusiness}");
