ba766ac9013a0cf91988c8e77ad890a4e049c591
Rielk <william@symbotics.co.uk>
2022-12-13 06:39:31 +0000

    Day 13 Part 1 - Parser

---
+++ AdventOfCode22Day13/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode22Day13;
+using AdventOfCode22Day13.Properties;
+
+string input = Resources.InputTest;
+
+List<ValuePair> Values = new();
+
+foreach (string pair in input.Split(Environment.NewLine + Environment.NewLine))
+{
+    string[] pairSplit = pair.Split(Environment.NewLine);
+    var value1 = ValueSet.CreateValueSet(pairSplit[0]);
+    var value2 = ValueSet.CreateValueSet(pairSplit[1]);
+    Values.Add(new(value1, value2));
+}
+
+Console.WriteLine();
+
+internal record ValuePair(ValueSet Value1, ValueSet Value2) { }
---
+++ AdventOfCode22Day13/ValueSet.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode22Day13;
+internal class ValueSet
+{
+    public object[] Values { get; }
+
+    private ValueSet(List<object> input)
+    {
+        if (!input.Any()) { Values = Array.Empty<object>(); return; }
+
+        List<object> GenValues = new();
+        List<object> buffer = new();
+        foreach (object item in input)
+        {
+            if (item is char c && c == ',')
+                ProcessAndClearBuffer();
+            else
+                buffer.Add(item);
+        }
+        ProcessAndClearBuffer();
+
+        Values = GenValues.ToArray();
+
+        void ProcessAndClearBuffer()
+        {
+            if (buffer.Count == 1 && buffer[0] is ValueSet vs)
+                GenValues.Add(vs);
+            else
+            {
+                int result = int.Parse(new string(buffer.Cast<char>().ToArray()));
+                GenValues.Add(result);
+            }
+            buffer.Clear();
+        }
+    }
+
+    public static ValueSet CreateValueSet(string input)
+    {
+        var InputState = input.ToCharArray().Cast<object>().ToList();
+
+        while (InputState.Any(x => x is char))
+        {
+            List<object> buffer = new();
+            int bufferStart = -1;
+            foreach ((object item, int index) in InputState.Select((x, i) => (x, i)))
+            {
+                if (item is char c1 && c1 == '[')
+                {
+                    bufferStart = index;
+                    buffer.Clear();
+                }
+                else if (item is char c2 && c2 == ']')
+                {
+                    InputState.RemoveRange(bufferStart, buffer.Count + 2);
+
+                    ValueSet replacement = new(buffer);
+
+                    InputState.Insert(bufferStart, replacement);
+                    break;
+                }
+                else
+                    buffer.Add(item);
+            }
+        }
+
+        if (InputState.Count < 1) throw new ArgumentException();
+
+        return (ValueSet)InputState[0];
+    }
+
+
+}
