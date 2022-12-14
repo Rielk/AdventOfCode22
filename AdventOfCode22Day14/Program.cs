9e63f439b2cf06cc7349b2e818ea987c92e432fc
Rielk <william@symbotics.co.uk>
2022-12-14 06:22:15 +0000

    Day 14 Part 2 - Complete

--- AdventOfCode22Day14/Program.cs
+++ AdventOfCode22Day14/Program.cs
@@ -44,38 +44,55 @@ foreach (string line in input.Split(Environment.NewLine))
 void MaxMin(int a, int b, out int max, out int min) => (max, min) = a > b ? (a, b) : (b, a);
 
 RunSand(out int SandAtRest);
-void RunSand(out int sandAtRest)
+ClearSand();
+RunSand(out int SandToFill, AbyssLevel + 2);
+
+
+Console.WriteLine($"Units of sand at rest: {SandAtRest}");
+Console.WriteLine();
+Console.WriteLine($"Units of sand to fill cavern: {SandToFill}");
+
+void RunSand(out int sandAtRest, int? floorLevel = null)
 {
     sandAtRest = 0;
-    const int startX = 500;
-    const int startY = 0;
-    int x = startX;
-    int y = startY;
-    while (y < AbyssLevel)
+    Location start = new(500, 0);
+    int x = start.x;
+    int y = start.y;
+    while ((floorLevel == null && y < AbyssLevel) || (floorLevel != null && !Filled.ContainsKey(start)))
     {
-        if (!Filled.ContainsKey(new(x, y + 1)))
+        if (floorLevel != null && y + 1 == floorLevel)
+        { }
+        else if (!Filled.ContainsKey(new(x, y + 1)))
         {
             y++;
+            continue;
         }
         else if (!Filled.ContainsKey(new(x - 1, y + 1)))
         {
             y++;
             x -= 1;
+            continue;
         }
         else if (!Filled.ContainsKey(new(x + 1, y + 1)))
         {
             y++;
             x += 1;
+            continue;
         }
-        else
-        {
-            Filled.Add(new(x, y), Solid.Sand);
-            sandAtRest++;
-            x = startX; y = startY;
-        }
+        Filled.Add(new(x, y), Solid.Sand);
+        sandAtRest++;
+        x = start.x; y = start.y;
     }
 }
-Console.WriteLine($"Units of sand at rest: {SandAtRest}");
+
+void ClearSand()
+{
+    foreach (KeyValuePair<Location, Solid> pair in Filled.ToArray())
+    {
+        if (pair.Value == Solid.Sand)
+            Filled.Remove(pair.Key);
+    }
+}
 
 public record Location(int x, int y) { }
 public enum Solid { Rock, Sand }
