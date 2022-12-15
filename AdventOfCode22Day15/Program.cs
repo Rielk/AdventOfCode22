3bdc3b421f69afed1c2e9eef27ff1b60410b534a
Rielk <william@symbotics.co.uk>
2022-12-15 07:05:21 +0000

    Day 15 Part 2 - Optimizing

--- AdventOfCode22Day15/Program.cs
+++ AdventOfCode22Day15/Program.cs
@@ -1,9 +1,10 @@
 ﻿using AdventOfCode22Day15;
 using AdventOfCode22Day15.Properties;
+using System.Numerics;
 
-const int CheckRow = 10;
-const int SearchRange = 20;
-string input = Resources.InputTest;
+const int CheckRow = 2000000;//10;
+const int SearchRange = 4000000;//20;
+string input = Resources.Input1;
 
 int minX = int.MaxValue;
 int maxX = int.MinValue;
@@ -32,30 +33,35 @@ foreach (string line in input.Split(Environment.NewLine))
     if (sensorLoc.x + distance > maxX) maxX = sensorLoc.x + distance;
 }
 
-int NotBeacon = 0;
-for (int i = minX; i <= maxX; i++)
-{
-    Location checkLoc = new(i, CheckRow);
-    if (InSensorRange(checkLoc))
-        NotBeacon++;
-}
-foreach (Beacon beacon in Sensors.Select(s => s.Beacon).Distinct())
-    if (beacon.Location.y == CheckRow)
-        NotBeacon--;
+int NotBeacon = Sensors.SelectMany(s => s.PointsAtY(CheckRow)).Distinct().Count();
+NotBeacon -= Sensors.Select(s => s.Beacon).Distinct().Where(b => b.Location.y == CheckRow).Count();
 
 Console.WriteLine($"Posiotion not beacon on line {CheckRow}: {NotBeacon}");
 Console.WriteLine();
 
+Location BeaconLoc = GetPossibleLocation();
+
+Location GetPossibleLocation()
+{
+    foreach (Sensor sensor in Sensors)
+        foreach (Location checkLoc in sensor.PointsNextToRegion())
+            if (checkLoc.x is >= 0 and <= SearchRange)
+                if (checkLoc.y is >= 0 and <= SearchRange)
+                    if (!InSensorRange(checkLoc))
+                        return checkLoc;
+    throw new Exception();
+}
+
+BigInteger TuningFrequency = (BigInteger)4000000 * BeaconLoc.x + BeaconLoc.y;
+Console.WriteLine($"Tuning Frequency: {TuningFrequency}");
+
+
+
 bool InSensorRange(Location loc)
 {
     foreach (Sensor sensor in Sensors)
-    {
         if (sensor.CheckInRange(loc))
             return true;
-    }
     return false;
 }
-
-
-Console.WriteLine($"{CheckLoc.x}");
 
--- AdventOfCode22Day15/Sensor.cs
+++ AdventOfCode22Day15/Sensor.cs
@@ -13,4 +13,30 @@ internal class Sensor
     public int Radius { get; }
 
     public bool CheckInRange(Location loc) => Location.DistanceTo(loc) <= Radius;
+
+    public IEnumerable<Location> PointsNextToRegion()
+    {
+        foreach (int yOff in Enumerable.Range(-Radius - 1, 2 * Radius + 2))
+        {
+            int xOff = Radius - Math.Abs(yOff) + 1;
+            yield return new(Location.x + xOff, Location.y + yOff);
+            if (xOff != 0)
+                yield return new(Location.x - xOff, Location.y + yOff);
+        }
+    }
+
+    public IEnumerable<Location> PointsAtY(int y)
+    {
+        int yOff = Math.Abs(y - Location.y);
+        if (yOff > Radius) yield break;
+
+        int xOff = Radius - yOff;
+        int xOffSave = xOff;
+
+        while (xOff >= -xOffSave)
+        {
+            yield return new(Location.x - xOff, y);
+            xOff--;
+        }
+    }
 }
