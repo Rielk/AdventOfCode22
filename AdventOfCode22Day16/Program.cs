2ea3ce8b571f32b3f65cb2e6d87ecf2812127988
Rielk <william@symbotics.co.uk>
2022-12-16 09:57:20 +0000

    Day 16 Part 2 - Complete

--- AdventOfCode22Day16/Program.cs
+++ AdventOfCode22Day16/Program.cs
@@ -21,20 +21,30 @@ Valve[] ValvesWithFR = Valves.Values.Where(v => v.FlowRate > 0).ToArray();
 
 Valve Start = Valves["AA"];
 List<Valve> Stack = new() { Start };
-int BestProduction = FindBestRoute(Stack, 30, out List<Valve> _);
+int BestProduction = FindBestRoute(Stack, 30, out _);
 Console.WriteLine($"Maximium possible production: {BestProduction}");
+
+int BestDoubleProduction = FindBestDoubleRoute(Stack, 26, out List<Valve> br1, out List<Valve> br2);
+Console.WriteLine($"Maximium possible production with 2: {BestDoubleProduction}");
 Console.WriteLine();
 
-int FindBestRoute(IEnumerable<Valve> initialStack, int totalTime, out List<Valve> bestRoute)
+int FindBestRoute(IEnumerable<Valve> initialStack, int totalTime, out List<Valve> bestRoute, IEnumerable<Valve>? excludePoints = null)
 {
     int remainingTime = totalTime - CalculateTimeUsed(initialStack);
     Valve current = initialStack.Last();
-    int maxProduction = CalculateProduction(initialStack, totalTime);
+    int maxProduction = int.MinValue;
     List<Valve>? bestStack = null;
-    foreach (Valve valve in ValvesWithFR.Where(v => !initialStack.Contains(v)).Where(v => current.DistanceTo(v) + 1 <= remainingTime))
+
+    IEnumerable<Valve> availableValves;
+    if (excludePoints != null)
+        availableValves = ValvesWithFR.Where(v => !excludePoints.Contains(v));
+    else
+        availableValves = ValvesWithFR;
+
+    foreach (Valve valve in availableValves.Where(v => !initialStack.Contains(v)).Where(v => current.DistanceTo(v) + 1 <= remainingTime))
     {
         IEnumerable<Valve> nextStack = initialStack.Append(valve);
-        int nextProduction = FindBestRoute(nextStack, totalTime, out List<Valve> nextRoute);
+        int nextProduction = FindBestRoute(nextStack, totalTime, out List<Valve> nextRoute, excludePoints);
         if (nextProduction > maxProduction)
         {
             bestStack = nextRoute;
@@ -42,6 +52,32 @@ int FindBestRoute(IEnumerable<Valve> initialStack, int totalTime, out List<Valve
         }
     }
     bestRoute = bestStack ?? initialStack.ToList();
+    return maxProduction < 0 ? CalculateProduction(bestRoute, totalTime) : maxProduction;
+}
+
+int FindBestDoubleRoute(IEnumerable<Valve> initialStack, int totalTime, out List<Valve> bestRoute1, out List<Valve> bestRoute2)
+{
+    int remainingTime = totalTime - CalculateTimeUsed(initialStack);
+    Valve current = initialStack.Last();
+
+    int maxProduction = CalculateProduction(initialStack, totalTime) + FindBestRoute(new Valve[] { initialStack.First() }, totalTime, out bestRoute2, initialStack);
+    List<Valve>? bestStack1 = null;
+    List<Valve>? bestStack2 = null;
+
+    foreach (Valve valve in ValvesWithFR.Where(v => !initialStack.Contains(v)).Where(v => current.DistanceTo(v) + 1 <= remainingTime))
+    {
+        IEnumerable<Valve> nextStack = initialStack.Append(valve);
+        int nextProduction = FindBestDoubleRoute(nextStack, totalTime, out List<Valve> nextRoute1, out List<Valve> nextRoute2);
+        if (nextProduction > maxProduction)
+        {
+            bestStack1 = nextRoute1;
+            bestStack2 = nextRoute2;
+            maxProduction = nextProduction;
+        }
+    }
+    bestRoute1 = bestStack1 ?? initialStack.ToList();
+    if (bestStack2 != null)
+        bestRoute2 = bestStack2;
     return maxProduction;
 }
 
@@ -59,7 +95,6 @@ int CalculateTimeUsed(IEnumerable<Valve> stack)
 
 int CalculateProduction(IEnumerable<Valve> stack, int totalTime)
 {
-    List<Valve> opened = new();
     int produced = 0;
     int production = 0;
     int timeRemaining = totalTime;
@@ -69,11 +104,7 @@ int CalculateProduction(IEnumerable<Valve> stack, int totalTime)
         int timePassed = current.DistanceTo(valve) + 1;
         timeRemaining -= timePassed;
         produced += production * timePassed;
-        if (!opened.Contains(valve))
-        {
-            production += valve.FlowRate;
-            opened.Add(valve);
-        }
+        production += valve.FlowRate;
         current = valve;
     }
     produced += production * timeRemaining;
